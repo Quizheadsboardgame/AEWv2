6a65c2f28266941fc664ac780a48542b7b79134d
Quizheadsboardgame <Minifiguresandmoreonline@gmail.com>
2024-09-25 19:20:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+function showAnswer() {
+    const answerElement = document.getElementById("answer");
+    answerElement.textContent = "2019"; // The answer to the question
+    answerElement.style.display = "block"; // Show the answer text
+}
 
